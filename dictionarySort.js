b8e61c54b3d7f5a3b2d2a5aa967454d3ce1f26d0
Susan Nio <susan.wen88@gmail.com>
2018-07-10 20:57:22 +0700

    Updating solution

--- dictionarySort.js
+++ dictionarySort.js
@@ -2,9 +2,9 @@ function dictionarySort(arr){
     for (let i = 1; i < arr.length; i++) {
         for (let j = i; j >= 0;j--) {
             if (arr[j] < arr[j-1]) {
-                let y = arr[j-1]
+                let x = arr[j-1]
                 arr[j-1] = arr[j]
-                arr[j] = y
+                arr[j] = x
             }
         }  
       }  
