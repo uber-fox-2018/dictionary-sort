cb7512728cf70da0a5924bd6084f1fa8e8151cb4
Andre Sudi Yudatama <andresudi@DDCBU-elee.localdomain>
2018-07-10 16:48:11 +0700

    dictionary sort with bubble sort algorithm

---
+++ dictionarySort.js
@@ -0,0 +1,17 @@
+function dictionarySorted(words) {
+
+    for (let i = 0; i < words.length; i++) {
+        for (let j = 0; j < words.length-1; j++) {
+            if (words[j] > words[j+1]) {
+                let temp = words[j]
+                words[j] = words[j+1]
+                words[j+1] = temp
+            }
+        }
+    }
+    return words.join(',')
+}
+
+console.log(dictionarySorted(["makan", "duduk", "tidur", "terbang"])); // duduk,makan,terbang,tidur
+console.log(dictionarySorted(["anggi", "angga", "ani", "adi"])); // adi,angga,anggi,ani
+//console.log(dictionarySorted([1, 6, 9, 5, 3, 4, 2, 12, 4567, 5, 34]))
