78d38119bbb9bf17fc2cd40d8aa30e3fb4817f9c
Andre Sudi Yudatama <andresudi@DDCBU-elee.localdomain>
2018-07-10 17:42:34 +0700

    revisi

--- dictionarySort.js
+++ dictionarySort.js
@@ -7,10 +7,8 @@ function dictionarySorted(words) {
             words[i+1] = temp
         }
     }
-   
     return words.join(',')
 }
 
 console.log(dictionarySorted(["makan", "duduk", "tidur", "terbang"])); // duduk,makan,terbang,tidur
-console.log(dictionarySorted(["anggi", "angga", "ani", "adi"])); // adi,angga,anggi,ani
-//console.log(dictionarySorted([1, 6, 9, 5, 3, 4, 2, 12, 4567, 5, 34]))
+console.log(dictionarySorted(["anggi", "angga", "ani", "adi"])); // angga,anggi,adi,ani
