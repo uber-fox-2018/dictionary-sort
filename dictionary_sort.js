d05e8e3cf11b3e06ec974d4e9c4357094b5d1af2
Odelia <odeliasusanto@gmail.com>
2018-07-10 17:20:01 +0700

    edited code to use only one looping for bubble sort

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,15 +1,15 @@
 function dictionarySort(arrOfWords) {
     for (let i = 0; i < arrOfWords.length; i++) {
-        for (let j = i+1; j < arrOfWords.length; j++) {
-            if (arrOfWords[j] < arrOfWords[i]) {
-                let temp = arrOfWords[j];
-                arrOfWords[j] = arrOfWords[i];
-                arrOfWords[i] = temp;
+        // for (let j = i+1; j < arrOfWords.length; j++) {
+            if (arrOfWords[i+1] < arrOfWords[i]) {
+                let temp = arrOfWords[i];
+                arrOfWords[i] = arrOfWords[i+1];
+                arrOfWords[i+1] = temp;
             }
-        }
+        // }
     }
     return arrOfWords;
 }
 
 console.log(dictionarySort(['makan', 'duduk', 'tidur', 'terbang'])); //duduk, makan, terbang, tidur
-console.log(dictionarySort(['anggi', 'angga', 'ani', 'adi'])); 
+console.log(dictionarySort(['anggi', 'angga', 'ani', 'adi'])); //angga, anggi, adi, ani
